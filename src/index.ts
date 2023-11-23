0c6239dce6feeda092a38563c65bc4adeccb4586
Anton Rusak <4231915+arusak@users.noreply.github.com>
2023-11-23 13:58:21 +0100

    enhanced functionality

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,55 @@
 import { Plugin, UserConfig, CSSOptions } from 'vite';
+import { createHash } from 'node:crypto';
 
 type PluginOptions = {
-  componentNameRegExp?: RegExp
+  componentNameMatching?: 'file' | 'path';
+  pathDepth?: number;
+  includeLineNumber?: boolean;
+  includeHash?: boolean;
+  separator?: string;
+  replacement?: string;
 }
 
-const defaultComponentNameRegExp = /\/([^/]*)\.module\.css$/
+const pathRegExp = /([^/]+)/g;
+const fileRegExp = /\/([^/]*)\.module\.css$/;
 
-export const readableCssModules = (options:PluginOptions = {}): Plugin => {
-  const {componentNameRegExp = defaultComponentNameRegExp} = options;
+const sanitize = (src: string, replacement: string) => {
+  return src.replace(/\W/g, replacement);
+};
+
+const hashText = (text: string) => createHash('md5').update(text).digest('base64url').substring(-4);
+
+const getComponentNameFromFile = (filename: string, replacement: string) => {
+  const matches = filename.match(fileRegExp);
+  let match = matches ? matches[1] : '';
+  return sanitize(match, replacement);
+};
+
+const getComponentNameFromPath = (filename: string, depth = Infinity, replacement: string, separator: string) => {
+  const pathElements = filename.match(pathRegExp);
+  if (!pathElements) {
+    return replacement;
+  }
+  pathElements.splice(-1);
+
+  const nameElements = pathElements.splice(-depth);
+  return nameElements.map(el => sanitize(el, replacement)).join(separator);
+};
+
+const calculateLineNumber = (css: string, className: string) => {
+  const i = css.indexOf('.' + className);
+  return css.substring(0, i).split(/[\r\n]/).length;
+};
+
+export const readableCssModules = (options: PluginOptions = {}): Plugin => {
+  const {
+    componentNameMatching = 'file',
+    pathDepth,
+    includeHash = false,
+    includeLineNumber = false,
+    replacement = '_',
+    separator = '_'
+  } = options;
 
   return ({
     name: 'readable-css-modules',
@@ -16,13 +58,19 @@ export const readableCssModules = (options:PluginOptions = {}): Plugin => {
       const cssOptions: CSSOptions = {
         modules: {
           generateScopedName:
-            (name, filename, css) => {
-              const i = css.indexOf('.' + name);
-              const line = css.substring(0, i).split(/[\r\n]/).length;
-              const matches = filename.match(componentNameRegExp);
-              const component = matches ? matches[1] : '';
+            (className, filename, cssSource) => {
+              const componentName = componentNameMatching === 'file'
+                ? getComponentNameFromFile(filename, replacement)
+                : getComponentNameFromPath(filename, pathDepth, replacement, separator);
 
-              return `${component}_${name}_${line}`;
+              return [
+                componentName,
+                className,
+                includeLineNumber && calculateLineNumber(cssSource, className),
+                includeHash && hashText(filename)
+              ]
+                .filter(Boolean)
+                .join(separator);
             },
         }
       };
